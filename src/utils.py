3c314e03d06c64d162939808a509fc53c2550af8
atest <ips@MacBookPro.attlocal.net>
2024-12-24 18:01:55 -0600

    prediction pipeline and app

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -54,6 +55,15 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
